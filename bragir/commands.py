efbf1b8e975073c81ea401d36c44007b26164e3b
Andreas Bergman <andreas@arjancodes.com>
2024-01-05 10:49:10 +0100

    Add directory option for transcribing videos, and smaller refactoring

--- bragir/commands.py
+++ bragir/commands.py
@@ -16,19 +16,27 @@ from bragir.file import (
 )
 from bragir.languages import Languages, parse_languages
 from bragir.messages import PROMPT_HELP
+from bragir.path import get_files_in_directory
 from bragir.path_components import File, SRTPart
 from bragir.time import update_timestamps
 from bragir.transcriber import transcribe_audio_files
 from bragir.translator import translate_srt
 
 
-@click.command(options_metavar='<options>')
+@click.command(options_metavar="<options>")
 @click.option(
     "--file_path",
     "-f",
-    required=True,
     type=click.Path(exists=True, file_okay=True),
     help=PROMPT_HELP["file_path"],
+    metavar="<path>",
+)
+@click.option(
+    "--directory_path",
+    "-d",
+    type=click.Path(exists=True, file_okay=True),
+    help=PROMPT_HELP["directory"],
+    metavar="<path>",
 )
 @click.option(
     "--output_path",
@@ -43,58 +51,83 @@ from bragir.translator import translate_srt
     envvar="OPENAI_API_KEY",
     help=PROMPT_HELP["api_key"],
 )
-def transcribe(file_path: str, output_path: str, api_key: str) -> None:
+def transcribe(
+    file_path: str, directory_path: str, output_path: str, api_key: str
+) -> None:
     """
     The transcribe command generates an SRT file based on an .mp4 or .mp3 file.
     If output is not set, then it will take the file_path name and change the extension.
     """
+    if not directory_path and not file_path:
+        click.echo("Please provide a file or directory")
+        exit(1)
+
     transcriber = initiate_client(api_key=api_key)
 
-    file_size_mbytes = calculate_file_size(file_path)
+    file_paths: list[str] = []
 
-    audio_paths: list[str] = [file_path]
-    if file_size_mbytes >= 25:
-        click.echo(f"Chunking {file_path} into smaller audio files.")
-        audio_paths = process_file(file_path)
+    if directory_path:
+        directory_file_paths = get_files_in_directory(directory_path)
+        file_paths = [*file_paths, *directory_file_paths]
 
-    click.echo(f"Transcribing {file_path}")
-    transcripts: list[str] = transcribe_audio_files(transcriber, audio_paths)
+    if file_path:
+        file_paths.append(file_path)
 
-    click.echo("Constructing SRT parts")
-    videos_srts: list[Tuple[int, list[SRTPart]]] = [
-        (order, chunk_content_into_srt_parts(transcript))
-        for order, transcript in enumerate(transcripts)
-    ]
+    click.echo(f"Processing {len(file_paths)} files")
 
-    click.echo("Updating timestamps")
-    sorted_videos = sorted(videos_srts)
+    tmp_audio_paths: list[str] = []
+    for path in file_paths:
+        file_size_mbytes = calculate_file_size(path)
 
-    srt_parts: list[SRTPart] = update_timestamps(sorted_videos)
+        if file_size_mbytes >= 25:
+            click.echo(f"Chunking {path} into smaller audio files.")
+            tmp_audio_paths = [*tmp_audio_paths, *process_file(path)]
 
-    click.echo("Creating SRT file content")
-    contents = "".join([srt_part.srt_format for srt_part in srt_parts])
+        click.echo(f"Transcribing {path}")
+        transcripts: list[str] = transcribe_audio_files(transcriber, tmp_audio_paths)
 
-    if output_path:
-        target_path = os.path.expanduser(output_path)
-    else:
-        root, _ = os.path.splitext(file_path)
-        target_path = root + ".srt"
+        click.echo("Constructing SRT parts")
+        videos_srts: list[Tuple[int, list[SRTPart]]] = [
+            (order, chunk_content_into_srt_parts(transcript))
+            for order, transcript in enumerate(transcripts)
+        ]
 
-    with open(target_path, "w", encoding="utf-8") as fileIO:
-        fileIO.write(contents)
-        click.echo(f"Created {target_path} for video {file_path}")
+        click.echo("Updating timestamps")
+        sorted_videos = sorted(videos_srts)
 
-    remove_files(audio_paths)
+        srt_parts: list[SRTPart] = update_timestamps(sorted_videos)
+
+        click.echo("Creating SRT file content")
+        contents = "".join([srt_part.srt_format for srt_part in srt_parts])
+
+        if output_path:
+            target_path = os.path.expanduser(output_path)
+        else:
+            root, _ = os.path.splitext(path)
+            target_path = root + ".srt"
+
+        with open(target_path, "w", encoding="utf-8") as fileIO:
+            fileIO.write(contents)
+            click.echo(f"Created {target_path} for video {path}")
+
+    remove_files(tmp_audio_paths)
 
 
-@click.command(options_metavar='<options>')
+@click.command(options_metavar="<options>")
 @click.option(
     "--file",
     "-f",
     type=click.Path(dir_okay=True, exists=True),
     prompt="Enter path to file",
     help=PROMPT_HELP["file"],
-    metavar='<path>'
+    metavar="<path>",
+)
+@click.option(
+    "--directory",
+    "-d",
+    type=click.Path(dir_okay=True, exists=True),
+    help=PROMPT_HELP["directory"],
+    metavar="<directory>",
 )
 @click.option(
     "--api_key",
@@ -104,19 +137,12 @@ def transcribe(file_path: str, output_path: str, api_key: str) -> None:
     help=PROMPT_HELP["api_key"],
 )
 @click.option(
-    "--directory",
-    "-d",
-    type=click.Path(dir_okay=True, exists=True),
-    help=PROMPT_HELP["directory"],
-    metavar='<directory>'
-)
-@click.option(
     "--language",
     "-l",
     required=True,
     type=click.Choice([language.value for language in Languages], case_sensitive=False),
     multiple=True,
-    help=PROMPT_HELP["language"]
+    help=PROMPT_HELP["language"],
 )
 def translate(file: str, api_key: str, language: str, directory: str) -> None:
     """
