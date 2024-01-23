ebe571bb00c94545ef116bbbbadc5c7181e82799
Andreas Bergman <andreas@arjancodes.com>
2024-01-23 16:06:40 +0100

    Remove unused files

--- tests/file_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from bragir.file import calculate_file_size
-
-
-def test_calculate_file_size_nonexistent_file():
-    # Test the calculate_file_size function with a non-existent file path
-    with pytest.raises(FileNotFoundError):
-        calculate_file_size("nonexistent_file.txt")
