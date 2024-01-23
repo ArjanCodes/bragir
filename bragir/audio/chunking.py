70306d6211c01753ce235fb5c02f0e9dcc73d185
Andreas Bergman <andreas@arjancodes.com>
2024-01-23 12:44:06 +0100

    Fix problem with creating to many files.
    Files are now grouped together to reach the maximum limit. This also prevents the files to be too small for whisper AI

--- bragir/audio/chunking.py
+++ bragir/audio/chunking.py
@@ -1,18 +1,18 @@
 from pydub import AudioSegment
-from pydub.silence import split_on_silence # type:ignore
-from bragir.audio.grouping import group_objects 
+from pydub.silence import split_on_silence  # type:ignore
+from bragir.audio.grouping import group_objects
+from bragir.constants import DURATION_SECONDS_25MB
 
-from bragir.constants import DURATION_SECONDS_25MB  # type:ignore
 
 def chunk_audio(file_path: str, format: str = "mp4") -> list[AudioSegment]:
     sound: AudioSegment = AudioSegment.from_file(  # type:ignore
-        file=file_path, format=format
+        file=file_path
     )
 
     chunks: list[AudioSegment] = split_on_silence(
         sound, min_silence_len=1000, silence_thresh=-40, keep_silence=True
     )  # type:ignore
 
-    limit = DURATION_SECONDS_25MB[format]
+    limit = DURATION_SECONDS_25MB[format] * 0.9
 
     return group_objects(chunks, limit)
--- bragir/audio/grouping.py
+++ bragir/audio/grouping.py
@@ -1,10 +1,11 @@
 from pydub import AudioSegment
 
+
 def combine(objects: list[AudioSegment]) -> AudioSegment | None:
     if len(objects) > 1:
         combined_no_crossfade = objects[0]
         for segment in objects[1:]:
-            combined_no_crossfade = combined_no_crossfade.append(segment, crossfade=0)
+            combined_no_crossfade += segment
         return combined_no_crossfade
 
     if len(objects) == 1:
@@ -13,17 +14,19 @@ def combine(objects: list[AudioSegment]) -> AudioSegment | None:
     return None
 
 
-def group_objects(
-    objects: list[AudioSegment], duration_limit_miliseconds: float
-) -> list[AudioSegment]:
-    duration_limit_seconds = duration_limit_miliseconds / 1000
+def audio_total_duration(objects: list[AudioSegment]) -> float:
+    return sum(obj.duration_seconds for obj in objects)
 
+
+def group_objects(
+    objects: list[AudioSegment], duration_limit_seconds: float
+) -> list[AudioSegment]:
     # If there is no objects in the list
     if len(objects) == 0:
         return []
-    
+
     # If AudioSegments are less than the limit
-    if combine(objects).duration_seconds < duration_limit_seconds:
+    if audio_total_duration(objects) < duration_limit_seconds:
         return [combine(objects)]
 
     grouped_objects: list[AudioSegment] = []
@@ -42,10 +45,8 @@ def group_objects(
 
     # Add the last group if it contains any objects
     if len(current_group) > 0:
-        combined_no_crossfade = current_group[0]
-        for segment in current_group[1:]:
-            combined_no_crossfade = combined_no_crossfade.append(segment, crossfade=0)
+        combined_no_crossfade = combine(current_group)
 
         grouped_objects.append(combined_no_crossfade)
 
-    return grouped_objects
+    return [item for item in grouped_objects if item is not None]
