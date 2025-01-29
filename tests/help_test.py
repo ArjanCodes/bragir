f034a4218cf3019291c2d4528eb36c660caa2b2c
Andreas Bergman <andreas@arjancodes.com>
2025-01-29 10:46:56 +0100

    Test: update tests to use fixtures instead

--- tests/help_test.py
+++ tests/help_test.py
@@ -1,14 +1,18 @@
+import pytest
 from click.testing import CliRunner
 from bragir.__main__ import cli
 
 
-def test_shorthand_help():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["-h"])
+@pytest.fixture
+def cli_runner():
+    return CliRunner()
+
+
+def test_shorthand_help(cli_runner: CliRunner):
+    result = cli_runner.invoke(cli, ["-h"])
     assert result.exit_code == 0
 
 
-def test_long_help():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--help"])
+def test_long_help(cli_runner: CliRunner):
+    result = cli_runner.invoke(cli, ["--help"])
     assert result.exit_code == 0
